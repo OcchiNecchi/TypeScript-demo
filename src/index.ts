1f16b30ca3098dc5863ca5fb8dacdd2aa51fcfff
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-09-07 00:49:10 +0900

    learn function

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,19 @@
 // world.sayHello(root);
 
 // 基本の型定義
-import { primitiveSample, anySample, unknownSample } from './basic';
+// import { primitiveSample, anySample, unknownSample } from './basic';
+// primitiveSample();
+// anySample();
+// unknownSample();
 
-primitiveSample();
+// 関数の型定義について
+import { logMessage } from './function/basic';
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters';
+logMessage('Hello TypeScript');
+isUserSignedIn('ABC', 'aaa');
+isUserSignedIn('DEF');
 
-anySample();
+isUserSignedIn2('ABC');
 
-unknownSample();
+const sum = sumProductsPrice(100, 200, 300, 400);
+console.log('Function parameters sample 5:', sum);
